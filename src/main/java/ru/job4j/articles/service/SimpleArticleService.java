839eb4edf7376f66ec5c497660795f4071b5eb93
paketchino <chansgorn@gmail.com>
2021-12-06 00:30:12 +0300

    Coomint

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -7,6 +7,8 @@ import ru.job4j.articles.model.Word;
 import ru.job4j.articles.service.generator.ArticleGenerator;
 import ru.job4j.articles.store.Store;
 
+import java.lang.ref.WeakReference;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -20,14 +22,30 @@ public class SimpleArticleService implements ArticleService {
         this.articleGenerator = articleGenerator;
     }
 
+    /**
+     * Исходный код
+     * var articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
+     *              .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
+     *              .mapToObj((x) -> articleGenerator.generate(words))
+     *              .collect(Collectors.toList());
+     *               articles.forEach(articleStore::save);
+     */
+
+    /**
+     * Второй Вариант
+     *  IntStream.iterate(0, i -> i < count, i -> i + 1)
+     *                 .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
+     *                 .mapToObj((x) -> articleGenerator.generate(words))
+     *                 .forEach(articleStore::save);
+     */
+
     @Override
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
         LOGGER.info("Геренация статей в количестве {}", count);
         var words = wordStore.findAll();
-        var articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
-                .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
-                .mapToObj((x) -> articleGenerator.generate(words))
-                .collect(Collectors.toList());
-        articles.forEach(articleStore::save);
+        for (int i = 0; i < count; i++) {
+            new WeakReference<>(articleStore.save(articleGenerator.generate(words)));
+            LOGGER.info("Сгенерирована статья № {}", i);
+        }
     }
 }
